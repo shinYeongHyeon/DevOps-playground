02b7badb37599ce80d29a761d8fbe7f330872cc7
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-04-23 22:01:43 +0900

    setup typeorm

--- smallArchitecture/api/src/app.module.ts
+++ smallArchitecture/api/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'saprootuser',
+      password: 'sappassword',
+      database: 'sap',
+      entities: [],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
