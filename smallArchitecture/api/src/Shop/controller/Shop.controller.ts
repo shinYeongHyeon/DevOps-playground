eb048d2424ee77ddf14d3f954d1bbc8c96e89a60
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-05-05 17:05:10 +0900

    Add Menu API

--- smallArchitecture/api/src/Shop/controller/Shop.controller.ts
+++ smallArchitecture/api/src/Shop/controller/Shop.controller.ts
@@ -4,21 +4,27 @@ import {
     Body,
     Controller,
     Get,
-    Param, Post,
+    Param,
+    Post,
 } from '@nestjs/common';
 
+import { MenuEntity } from '../entities/Menu.entity';
 import { ShopEntity } from '../entities/Shop.entity';
 
 @Controller('shops')
 export class ShopController {
     constructor(
         @InjectRepository(ShopEntity)
-        private readonly shopRepository: Repository<ShopEntity>
+        private readonly shopRepository: Repository<ShopEntity>,
+        @InjectRepository(MenuEntity)
+        private readonly menuRepository: Repository<MenuEntity>,
     ) {}
 
     @Get('')
     async findAll(): Promise<ShopEntity[]> {
-        return await this.shopRepository.find();
+        return await this.shopRepository.find({
+            relations: ['menus']
+        });
     }
 
     @Get(':id')
@@ -34,9 +40,48 @@ export class ShopController {
             ...saveShopDto
         })
     }
+
+    @Get(':shopId/menus')
+    async findAllMenus(): Promise<MenuEntity[]> {
+        return await this.menuRepository.find();
+    }
+
+    @Get(':shopId/menus/:menuId')
+    async findMenu(
+        @Param('shopId')
+        shopId: number,
+        @Param('menuId')
+        menuId: number,
+    ): Promise<MenuEntity> {
+        return await this.menuRepository.findOneBy({
+            id: menuId,
+            shop: await this.shopRepository.findOneBy({
+                id: shopId,
+            })
+        })
+    }
+
+    @Post(':shopId/menus')
+    async saveMenu(
+        @Param('shopId')
+        shopId: number,
+        @Body()
+        saveMenuDto: SaveMenuDto,
+    ): Promise<void> {
+        await this.menuRepository.save({
+            foodName: saveMenuDto.foodName,
+            shop: await this.shopRepository.findOneBy({
+                id: shopId,
+            }),
+        })
+    }
 }
 
 interface SaveShopDto {
     name: string;
     address: string;
+}
+
+interface SaveMenuDto {
+    foodName: string;
 }
