3caa088721e6f70dc6e88bbf55e443dc810adce1
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-05-12 23:13:48 +0900

    EC2 & Deploy

--- smallArchitecture/api/README.md
+++ smallArchitecture/api/README.md
@@ -1,4 +1,15 @@
 # 소규모 아키텍쳐 구성
 
+## Instance
+- EC2 (ubuntu 22)
+
 ## Repository
 - RDS (postgresql)
+
+### 단순배포
+- instance 안에서 `ssh-keygen -t rsa`
+- `cat ~/.ssh/id_rsa.pub` 를 복사해서 Git Repository `Settings > Deploy keys`에 추가
+- 다시 instance 안에서 `ssh-keygen -t rsa -C "{깃헙이메일주소}"`
+- overwrite 하면되고, `cat ~/.ssh/id_rsa.pub` 복사해서 깃헙 계정 프로필에 SSH 키로 등록
+- 그러고 나서 `git clone {레포주소}`
+- START !
--- smallArchitecture/api/src/app.module.ts
+++ smallArchitecture/api/src/app.module.ts
@@ -1,24 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { BossModule } from './Boss/Boss.module';
 import { DeliveryModule } from './Delivery/Delivery.module';
-
 import { OrderModule } from './Order/Order.module';
+import { ShopModule } from './Shop/Shop.module';
 import { OrderEntity } from './Order/entities/Order.entity';
 import { OrderFoodEntity } from './Order/entities/OrderFood.entity';
-import { ShopModule } from './Shop/Shop.module';
 import { MenuEntity } from './Shop/entities/Menu.entity';
 import { ShopEntity } from './Shop/entities/Shop.entity';
 
-// TODO: DB 정보 삭제 (인프라 집중이기 때문에, 굳이 config 까지는...)
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '',
+      host: 'small-architecture-postgre.chebidgehwba.us-west-2.rds.amazonaws.com',
       port: 5432,
-      username: '',
-      password: '',
+      username: 'postgres',
+      password: 'postgrespwd!',
       database: 'sap',
       entities: [
           ShopEntity,
